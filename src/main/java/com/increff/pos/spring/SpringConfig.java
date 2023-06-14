18a9b62b36d6b5700d0b2be0faa1367e03aee7b9
rounak Agrawal <rounkagrwl@gmail.com>
2023-06-14 21:01:08 +0530

    "[update] - configuration"

--- src/main/java/com/increff/pos/spring/SpringConfig.java
+++ src/main/java/com/increff/pos/spring/SpringConfig.java
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
 @Configuration
-@ComponentScan("com.increff.employee")
+@ComponentScan("com.increff.pos")
 @PropertySources({ //
-		@PropertySource(value = "file:./employee.properties", ignoreResourceNotFound = true) //
+		@PropertySource(value = "file:./pos.properties", ignoreResourceNotFound = true) //
 })
 public class SpringConfig {
 
